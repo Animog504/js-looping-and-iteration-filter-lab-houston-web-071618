b6867e8a0ef48c69df9745cd4a34a257b24febab
Flatiron School <flatironschool@Flatirons-MacBook-Air-3.local>
2018-08-27 14:57:50 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file
+function findMatching(collection, name)
+{
+  return result = collection.filter(function(PersonName){
+    return PersonName.toLowerCase() === name.toLowerCase();
+  });
+}
+function fuzzyMatch(collection, letters)
+{
+  //so it returns an array where the letters passed match the names the collection contains
+  return result = collection.filter(function(personName){
+    return personName.slice(0,letters.length) === letters
+  });
+}
+function matchName(collection, name)
+{
+  return result = collection.filter(function(person){
+    return person.name.toLowerCase() === name.toLowerCase()
+  });
+}
